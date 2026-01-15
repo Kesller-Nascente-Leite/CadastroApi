e55e2db5bc9cdd504c40bbb608ce51a5b91d69e0
Kesller Nascente Leite <165293702+Kesller-Nascente-Leite@users.noreply.github.com>
2026-01-15 14:22:25 -0300

    Terminando cursinho do Youtube do programador Fiasco.

--- src/main/java/com/example/CadastroAPI/NInjas/NinjaModel.java
+++ src/main/java/com/example/CadastroAPI/NInjas/NinjaModel.java
@@ -2,12 +2,17 @@ package com.example.CadastroAPI.NInjas;
 
 import com.example.CadastroAPI.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 //O Entity transforma uma classe em uma entidade do BD
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+//@Getter ou @Setter
+@Data // cria os Getter e Setter
 public class NinjaModel {
 
     @Id
@@ -21,36 +26,5 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") // Foreing key
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
 
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
